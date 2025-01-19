1af1bc3b092af64bf63a31d067ff5f73a568fdc2
João <joao.mello.100@ufrn.edu.br>
2025-01-19 17:43:54 -0300

    Fix CORS

---
+++ src/main/java/com/webii/comandas/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.webii.comandas.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Permite todas as rotas
+                        .allowedOrigins("http://localhost:3000") // Permite a origem do front-end
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                        .allowedHeaders("*") // Permite todos os cabeçalhos
+                        .allowCredentials(true); // Permite envio de cookies
+            }
+        };
+    }
+}
